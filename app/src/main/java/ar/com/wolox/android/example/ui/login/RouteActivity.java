94fd031e05dfd5f926ca130e862b7d3c9580cb2b
pfernandez <patricio.fernandez@wolox.com.ar>
2019-04-23 13:00:51 -0300

    Se creo la clase RouteActivity donde se valida si hay una sesion iniciada y segun el resultado inicia la LoginActivity. Se valida q los campos no esten incompletos y el mail tenga un formato válido

---
+++ app/src/main/java/ar/com/wolox/android/example/ui/login/RouteActivity.java
@@ -0,0 +1,37 @@
+package ar.com.wolox.android.example.ui.login;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+import ar.com.wolox.android.R;
+import ar.com.wolox.wolmo.core.activity.WolmoActivity;
+
+/**
+ *
+ */
+public class RouteActivity extends WolmoActivity {
+
+    @Override
+    protected int layout() {
+        return R.layout.activity_base;
+    }
+
+    @Override
+    protected void init() {
+        if (readSharedPreferences().equals("")) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Usuario logueado", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String readSharedPreferences() {
+        SharedPreferences sharedPref = this.getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        return sharedPref.getString(getString(R.string.saved_user_email), "");
+    }
+}
